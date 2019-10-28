1caa5aeafeb89c8df6fc48f4a64889e05e254ad0
[Lilgee147] <[onuadobunu@gmail.com]>
2019-10-28 10:37:31 +0100

    made changes

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function btnPlus() {
+    document.Calculator.display.value += "+";
+}
+
+function btnMinus() {
+    document.Calculator.display.value += "-";
+}
+
+function btnMultiply() {
+    document.Calculator.display.value += "*";
+}
+
+function btnDivide() {
+    document.Calculator.display.value += "/";
+}
+
+function btnMod() {
+    document.Calculator.display.value += "%";
+}
+
+function btnDot() {
+    document.Calculator.display.value += ".";
+}
